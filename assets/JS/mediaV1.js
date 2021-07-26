a99de70e2d8b836a6480f310bb905ac437ab4262
Clydic <70671047+Clydic@users.noreply.github.com>
2021-07-26 13:02:10 +0200

    sauvegarde

--- assets/JS/mediaV1.js
+++
@@ -1,9 +0,0 @@
-var media = [
-	{file : 'Blue_Skies', name : 'Blue Skies'},
-	{file : 'Cartoon_Hoedown', name : 'Cartoon Hoedown'},
-	{file : 'Earthy_Crust', name : 'Earthy Crust'},
-	{file : 'Hold_On_a_Minute', name : 'Hold On a Minute'},
-	{file : 'JohnDunbarTheme', name : 'John Dunbar Thème'},
-	{file : 'Stay_With_You', name : 'Stay With You'},
-	{file : 'Symphony_No_5_by_Beethoven', name : 'Symphony No 5 by Beethoven'}
-];
